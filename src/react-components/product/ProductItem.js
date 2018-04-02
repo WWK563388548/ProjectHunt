5638adc241eb30652afaf25855c8507c3c7ecc22
WWK563388548 <wc563388548@gmail.com>
2018-04-02 18:35:06 +0900

    refactored code of productItem component

--- src/react-components/product/ProductItem.js
+++ src/react-components/product/ProductItem.js
@@ -1,31 +1,49 @@
 import React from 'react';
 
 class ProductItem extends React.Component {
+
+    renderUpvoteButton() {
+        return (
+            <a className="upvote-button" href="#">
+                <span>
+                    <i className="fas fa-thumbs-up"></i>
+                </span>
+                {this.props.upvote}
+            </a>
+        );
+    }
+
+    renderNewWindowLinkIcon() {
+        return (
+            <a className="product-item-link" href={this.props.link}>
+                <span>
+                    <i className="fas fa-external-link-alt"></i>
+                </span>
+            </a>        
+        );
+    }
+
+    renderInfoSection() {
+        return (
+            <section className="product-item-info">
+                <a href="#">
+                    <h2>{this.props.name}</h2>
+                </a>
+                <p>{this.props.description}</p>
+                <a href="#">
+                    <img className="small-avatar" src={this.props.maker.avatar} />
+                </a>        
+            </section>   
+        );
+    }
+
     render() {
         return (
             <li className="product-item">
-                <a className="upvote-button" href="#">
-                    <span>
-                        <i className="fas fa-thumbs-up"></i>
-                    </span>
-                    {this.props.upvote}
-                </a>
+                {this.renderUpvoteButton()}
                 <img className="product-item-media" src={this.props.media} />
-                <section className="product-item-info">
-                    <a href="#">
-                        <h2>{this.props.name}</h2>
-                    </a>
-                    <p>{this.props.description}</p>
-                    <a href="#">
-                        <img className="small-avatar" src={this.props.maker.avatar} />
-                    </a>
-                            
-                </section>
-                <a className="product-item-link" href={this.props.link}>
-                    <span>
-                        <i className="fas fa-external-link-alt"></i>
-                    </span>
-                </a>
+                {this.renderInfoSection()}
+                {this.renderNewWindowLinkIcon()}
             </li>            
         );
     }
