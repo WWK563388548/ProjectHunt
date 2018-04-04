f4bcd86302903c9dd6c5bfe472d1649b7ac52c33
WWK563388548 <wc563388548@gmail.com>
2018-04-04 23:14:03 +0900

    created popup component

---
+++ src/react-components/navbar/Popup.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class Popup extends React.Component {
+
+    renderPopupContent() {
+        return (
+            <section className="popup">
+                <section className="popup-wrap">
+                    <img src="/img/close.png" />
+                </section>
+                <section className="popup-content">
+                    <section>Content should be here</section>
+                </section>
+            </section>
+        );
+    }
+    
+    render() {
+        return(
+            <section>
+                {this.renderPopupContent()}
+            </section>
+        );
+    }
+}
+
+export default Popup;
